fd2985a3a224520a1c641e3cc9b90e2341369368
V V PAVITRA HANUMASRI <163735552+pavitravedulla@users.noreply.github.com>
2025-09-20 10:18:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
    base: '/hotelbooking/',
+  server: {
+    proxy: {
+      '/back1': 'http://localhost:8080',
+      '/back3': 'http://localhost:8081'
+    }
+  }
 })
